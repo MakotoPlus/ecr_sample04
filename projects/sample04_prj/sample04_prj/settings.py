a82120f01a76e5e130b528ebfebfb6c9b030bb33
fukupee <fukupeemail@gmail.com>
2022-07-09 17:02:53 +0900

    Buildファイル保存

--- projects/sample04_prj/sample04_prj/settings.py
+++ projects/sample04_prj/sample04_prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1$-o!hg)3xt@ua45alkku^9hfg7gquf07hxqp#*654&m5(f#v!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
